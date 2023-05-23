7583c29e00fd2a7edfd734e63b2320313e07f81d
bsmoon-86 <ekflafg@naver.com>
2023-05-23 14:51:50 +0900

    Update token.js

--- token_trade/token.js
+++ token_trade/token.js
@@ -30,4 +30,34 @@ async function create_token(_name, _symbol, _decimal, _amount){
     console.log(kip7._address)
 }
 
-create_token('Test', 'TS', 0, 100000000)
+// 토큰 생성 함수 호출
+// create_token('Test', 'TS', 0, 100000000)
+
+// 토큰을 거래하는 함수 
+async function trans_token(_address, _amount){
+    // 발행한 토큰의 주소를 keyringContainer에 등록
+    const kip7 = await new caver.kct.kip7('0x8475da976e3A3E34882B7b3445da5A5D3c6d537F')
+    kip7.setWallet(keyringContainer)
+
+    // 발행한 토큰을 매개변수 _address에게 _amount 만큼 보내준다. 
+    const receipt = await kip7.transfer(
+        _address, 
+        _amount, 
+        {from : keyring.address})
+
+    console.log(receipt)
+}
+
+// trans_token('0xd52863320168D36402EFb31b36515D723656258D', 10000)
+
+
+// 유저가 토큰 발행자에게 토큰을 보내는 함수
+async function trans_from_token(_address, _amount){
+    // 발행한 토큰의 주소를 keyringContainer에 등록
+    const kip7 = await new caver.kct.kip7('0x8475da976e3A3E34882B7b3445da5A5D3c6d537F')
+    kip7.setWallet(keyringContainer)  
+
+    // 토큰 발행자의 지갑 주소
+    
+
+}
